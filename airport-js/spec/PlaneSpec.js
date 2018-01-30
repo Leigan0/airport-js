6fd71a72fe6155528b5144110483f7302569a290
Leigh-ann <leighann.gant@gmail.com>
2018-01-30 22:23:41 +0000

    plane raises errors land / take off

--- airport-js/spec/PlaneSpec.js
+++ airport-js/spec/PlaneSpec.js
@@ -15,12 +15,20 @@ describe('Plane',function(){
       plane.land()
       expect(plane.isFlying).toBe(false)
     })
+    it('will raise error if plane is not flying', function(){
+      plane.land()
+      expect(function() { plane.land()}).toThrow("Plane is not in the air")
+    })
   })
 
   describe('#takeOff', function() {
     it('is now flying', function() {
+      plane.land()
       plane.takeOff()
       expect(plane.isFlying).toBe(true)
+    })
+    it('will raise error if plane is not grounded', function(){
+      expect(function() { plane.takeOff()}).toThrow("Plane is not on the ground")
     })
   })
 })
--- airport-js/src/Plane.js
+++ airport-js/src/Plane.js
@@ -3,9 +3,11 @@ function Plane() {
 }
 
 Plane.prototype.land = function() {
+  if (!this.isFlying) throw "Plane is not in the air"
   this.isFlying = false
 }
 
 Plane.prototype.takeOff = function() {
+  if (this.isFlying) throw "Plane is not on the ground"
   this.isFlying = true
 }
