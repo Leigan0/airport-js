4bc3244d378267e2c001e3949bfb3d5564a0a52b
Leigh-ann <leighann.gant@gmail.com>
2018-01-30 20:33:14 +0000

    Takeoff and landing now throw errors - objects mocked out in tests

--- airport-js/spec/AirportSpec.js
+++ airport-js/spec/AirportSpec.js
@@ -1,47 +1,59 @@
 describe('Airport',function(){
   var airport;
   var plane;
+  var weather;
 
   beforeEach(function(){
-    airport = new Airport();
-    plane = {
-      land: function(){
-      },
-      takeOff: function(){
-      }
-    }
-  });
+    weather = jasmine.createSpyObj('weather',['isStormy'])
+    airport = new Airport(weather);
+    plane = jasmine.createSpyObj('plane', ['land', 'takeOff'])
+  })
 
-  describe('#land', function(){
+  it('has no planes by default',function(){
+    expect(airport.planes).toEqual([])
+  })
+
+  describe('under normal conditions', function(){
+
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(false);
+    })
+
     it("it lands a plane", function(){
       expect(airport.land(plane)).toEqual(plane)
     })
     it("calls land on plane obj",function(){
-      spyOn(plane,'land')
       airport.land(plane)
       expect(plane.land).toHaveBeenCalled();
     })
-  })
-
-  describe('#planes', function(){
     it("it stores planes", function(){
       airport.land(plane)
       expect(airport.planes).toContain(plane)
     })
-  })
-
-  describe('#take_off', function(){
     it("removes plane from planes", function(){
       airport.land(plane)
-      airport.take_off(plane)
+      airport.takeOff(plane)
       expect(airport.planes).toEqual([])
     })
-
     it("calls takeOff on plane obj", function(){
-      spyOn(plane,'takeOff')
       airport.land(plane)
-      airport.take_off(plane)
+      airport.takeOff(plane)
       expect(plane.takeOff).toHaveBeenCalled();
+    })
+  })
+
+  describe('under stormy conditions', function(){
+
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(true);
+    })
+
+    it('will not allow plane to land in stormy weather', function(){
+      expect(function() { airport.land(plane)}).toThrow("Unable to land, weather is stormy")
+    })
+
+    it('will not allow a plane to take off in stormy weather', function(){
+      expect(function() { airport.takeOff(plane)}).toThrow("Unable to take off, weather is stormy")
     })
   })
 })
--- airport-js/src/Airport.js
+++ airport-js/src/Airport.js
@@ -1,14 +1,21 @@
-function Airport () {
+function Airport (weather) {
   this.planes = []
+  this.weather = typeof weather !== 'undefined' ? weather : new Weather();
 }
 
 Airport.prototype.land = function(plane) {
+  if (this.weather.isStormy()) {
+    throw "Unable to land, weather is stormy"
+  }
   this.planes.push(plane)
   plane.land()
   return plane
 }
 
-Airport.prototype.take_off = function(plane) {
+Airport.prototype.takeOff = function(plane) {
+  if (this.weather.isStormy()) {
+    throw "Unable to take off, weather is stormy"
+  }
   var index = this.planes.indexOf(plane);
   this.planes.splice(index,1)
   plane.takeOff()
