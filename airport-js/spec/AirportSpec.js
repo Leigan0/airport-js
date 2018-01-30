1757fae8c201192f5717aeb9f35b9399c90daf17
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-30 14:24:27 +0000

    airport stores planes

--- airport-js/spec/AirportSpec.js
+++ airport-js/spec/AirportSpec.js
@@ -6,9 +6,17 @@ describe('Airport',function(){
     airport = new Airport();
     plane = "plane"
   });
+
   describe('#land', function(){
     it("it lands a plane", function(){
       expect(airport.land(plane)).toEqual(plane)
+    })
+  })
+
+  describe('#planes', function(){
+    it("it stores planes", function(){
+      airport.land(plane)
+      expect(airport.hangar()).toContain(plane)
     })
   })
 })
--- airport-js/src/Airport.js
+++ airport-js/src/Airport.js
@@ -1,7 +1,12 @@
 function Airport () {
-
+  this.planes = []
 }
 
 Airport.prototype.land = function(plane) {
+  this.planes.push(plane)
   return plane
+}
+
+Airport.prototype.hangar = function() {
+  return this.planes
 }
