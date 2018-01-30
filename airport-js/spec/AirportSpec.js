a5c4ba59e7051cac5aad593a06601c0059b24237
Leigh-ann <leighann.gant@gmail.com>
2018-01-30 13:02:17 +0000

    First test written

--- airport-js/spec/AirportSpec.js
+++ airport-js/spec/AirportSpec.js
@@ -1,1 +1,14 @@
+describe('Airport',function(){
+  var airport;
+  var plane;
 
+  beforeEach(function(){
+    airport = new Airport();
+    plane = "plane"
+  });
+  describe('#land', function(){
+    it("it lands a plane", function(){
+      expect(airport.land(plane)).toEqual(plane)
+    })
+  })
+})
